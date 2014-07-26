d700c782dc589de955fbef78f3cb68a98afc0a7d
Jaroslaw Pawlak <jpawlak7@gmail.com>
2014-07-26 11:29:16 +0100

    added acceptance test for sized query

--- src/test-acceptance/java/main/NewsFeedTest.java
+++ src/test-acceptance/java/main/NewsFeedTest.java
@@ -34,4 +34,27 @@ public class NewsFeedTest {
         assertThat(newsFeed, is(sameBeanAs(Arrays.asList(msg1, msg2, msg3, msg4, msg5))));
     }
 
+    @Test
+    public void returnsLastFivePostedMessages() {
+        Message msg1 = new Message(10, "msg1");
+        Message msg2 = new Message(20, "msg2");
+        Message msg3 = new Message(30, "msg3");
+        Message msg4 = new Message(40, "msg4");
+        Message msg5 = new Message(50, "msg5");
+        Message msg6 = new Message(60, "msg6");
+        Message msg7 = new Message(70, "msg7");
+
+        application.post(userId, msg1);
+        application.post(userId, msg2);
+        application.post(userId, msg3);
+        application.post(userId, msg4);
+        application.post(userId, msg5);
+        application.post(userId, msg6);
+        application.post(userId, msg7);
+
+        List<Message> newsFeed = application.getNewsFeed(userId, 5);
+
+        assertThat(newsFeed, is(sameBeanAs(Arrays.asList(msg3, msg4, msg5, msg6, msg7))));
+    }
+
 }
