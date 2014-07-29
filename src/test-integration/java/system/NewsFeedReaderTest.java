36ddfd6aec894d6c24e4b130bca4c9b0cf8cad63
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-07-29 12:24:12 +0100

    3.1 created integration test for NewsFeedReader

---
+++ src/test-integration/java/system/NewsFeedReaderTest.java
@@ -0,0 +1,30 @@
+package system;
+
+import org.junit.Test;
+
+import static com.shazam.shazamcrest.MatcherAssert.assertThat;
+import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
+import static org.hamcrest.CoreMatchers.is;
+
+public class NewsFeedReaderTest {
+
+    private final UserId userId1 = new UserId(56);
+    private final UserId userId2 = new UserId(97);
+
+    private final NewsFeedReader newsFeedReader = new NewsFeedReader();
+
+    @Test
+    public void persistsNewsFeedIntoDb() {
+        Message msg1 = new Message(7, "user 1, message 2");
+        Message msg2 = new Message(9, "user 2, message 1");
+        Message msg3 = new Message(9, "user 2, message 2");
+
+        newsFeedReader.post(userId1, msg1);
+        newsFeedReader.post(userId2, msg2);
+        newsFeedReader.post(userId2, msg3);
+
+        assertThat(newsFeedReader.getNewsFeed(userId1), is(sameBeanAs(new NewsFeed(msg1))));
+        assertThat(newsFeedReader.getNewsFeed(userId2), is(sameBeanAs(new NewsFeed(msg2, msg3))));
+    }
+
+}
