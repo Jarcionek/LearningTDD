dca50edccad8385b5ca961f8cca502814c128958
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-07-29 12:45:48 +0100

    3.8 updated acceptance test so it fails more often now

--- src/test-acceptance/java/system/NewsFeedTest.java
+++ src/test-acceptance/java/system/NewsFeedTest.java
@@ -22,13 +22,19 @@ public class NewsFeedTest {
     public void retrievesEntireNewsFeed() {
         Message msg1 = new Message(15, "msg1");
         Message msg2 = new Message(25, "msg2");
+        Message msg3 = new Message(40, "msg3");
+        Message msg4 = new Message(50, "msg4");
+        Message msg5 = new Message(90, "msg5");
 
         application.post(userId, msg1);
         application.post(userId, msg2);
+        application.post(userId, msg3);
+        application.post(userId, msg4);
+        application.post(userId, msg5);
 
         NewsFeed newsFeed = application.getNewsFeed(userId);
 
-        assertThat(newsFeed, is(sameBeanAs(new NewsFeed(msg1, msg2))));
+        assertThat(newsFeed, is(sameBeanAs(new NewsFeed(msg1, msg2, msg3, msg4, msg5))));
     }
 
 }
