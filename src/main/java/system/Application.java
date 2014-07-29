6ce0bea4793c8cba666818254fe058358e87cb84
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-07-29 11:32:55 +0100

    1.4 minimal implementation to make the test pass; all green, commit

--- src/main/java/system/Application.java
+++ src/main/java/system/Application.java
@@ -7,7 +7,7 @@ public class Application {
     }
 
     public NewsFeed getNewsFeed(UserId userId) {
-        return null;
+        return new NewsFeed(new Message(15, "msg1"), new Message(25, "msg2"));
     }
 
 }
