e65e1a6a68052e9821c750009ae9fdf8dcf7940f
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-07-29 12:09:17 +0100

    2.1 created unit test for Application

---
+++ src/test/java/system/ApplicationTest.java
@@ -0,0 +1,39 @@
+package system;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static com.shazam.shazamcrest.MatcherAssert.assertThat;
+import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ApplicationTest {
+
+    private static final UserId USER_ID = new UserId(3);
+
+    @Mock private NewsFeedReader newsFeedReader;
+
+    private Application application;
+
+    @Before
+    public void setup() {
+        application = new Application(newsFeedReader);
+    }
+
+    @Test
+    public void delegatesGetQueryToNewsFeedReader() {
+        NewsFeed expectedNewsFeed = new NewsFeed(new Message(90, "M1"), new Message(100, "M2"));
+
+        when(newsFeedReader.getNewsFeed(USER_ID)).thenReturn(expectedNewsFeed);
+
+        NewsFeed actualNewsFeed = application.getNewsFeed(USER_ID);
+
+        assertThat(actualNewsFeed, is(sameBeanAs(expectedNewsFeed)));
+    }
+
+}
