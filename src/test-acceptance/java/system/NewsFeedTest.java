831b108dbbd1f9701856ec7b2a9ec4b611ce2728
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-07-31 15:24:37 +0100

    added more data to acceptance test

--- src/test-acceptance/java/system/NewsFeedTest.java
+++ src/test-acceptance/java/system/NewsFeedTest.java
@@ -21,13 +21,19 @@ public class NewsFeedTest {
     public void retrievesAllPostedMessages() {
         Message msg1 = new Message(20, "msg1");
         Message msg2 = new Message(30, "msg2");
+        Message msg3 = new Message(40, "msg3");
+        Message msg4 = new Message(50, "msg4");
+        Message msg5 = new Message(60, "msg5");
 
         application.post(userId, msg1);
         application.post(userId, msg2);
+        application.post(userId, msg3);
+        application.post(userId, msg4);
+        application.post(userId, msg5);
 
         NewsFeed actualNewsFeed = application.getNewsFeed(userId);
 
-        assertThat(actualNewsFeed, is(sameBeanAs(new NewsFeed(msg1, msg2))));
+        assertThat(actualNewsFeed, is(sameBeanAs(new NewsFeed(msg1, msg2, msg3, msg4, msg5))));
     }
 
 }
